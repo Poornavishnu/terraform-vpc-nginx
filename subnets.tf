db4fc67e6c58e2c970ce42b3906455dc2f80482e
poornavishnu <4vishnug@gmail.com>
2025-08-21 17:05:36 -0400

    created public and private subnets across 3 AZs

---
+++ subnets.tf
@@ -0,0 +1,26 @@
+resource "aws_subnet" "public_subnets" {
+  count                   = 3
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = cidrsubnet("10.0.0.0/16", 8, count.index)
+  availability_zone       = data.aws_availability_zones.available.names[count.index]
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "public-subnet-${count.index + 1}"
+  }
+}
+
+resource "aws_subnet" "private_subnets" {
+  count             = 3
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = cidrsubnet("10.0.0.0/16", 8, count.index + 3)
+  availability_zone = data.aws_availability_zones.available.names[count.index]
+
+  tags = {
+    Name = "private-subnet-${count.index + 1}"
+  }
+}
+
+data "aws_availability_zones" "available" {
+  state = "available"
+}
