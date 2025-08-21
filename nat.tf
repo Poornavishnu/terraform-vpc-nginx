86be524c5b4e5ceb25709ba73cb83d6bcee2140c
poornavishnu <4vishnug@gmail.com>
2025-08-21 17:07:21 -0400

    added NAT Gateway and EIP for private subnet internet access

---
+++ nat.tf
@@ -0,0 +1,18 @@
+resource "aws_eip" "nat_eip" {
+  domain = "vpc"
+
+  tags = {
+    Name = "nat-eip"
+  }
+}
+
+resource "aws_nat_gateway" "nat_gw" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnets[0].id
+
+  tags = {
+    Name = "nat-gateway"
+  }
+
+  depends_on = [aws_internet_gateway.igw]
+}
